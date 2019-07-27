a63013fc81c0d113f24052f6f1ae60f75a72e202
Colten Krauter <coltenkrauter@gmail.com>
2019-07-26 21:01:49 -0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'angular-ssr-example';
+  title = 'Angular SSR Example';
+  production = environment.production;
 }
